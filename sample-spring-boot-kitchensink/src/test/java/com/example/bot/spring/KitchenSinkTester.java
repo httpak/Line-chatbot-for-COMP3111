ce691cdfb6934df1f18fe73d93dfccb6028ff0a4
httpak <httpak@connect.ust.hk>
2017-09-22 16:40:44 +0800

    Test #2

--- sample-spring-boot-kitchensink/src/test/java/com/example/bot/spring/KitchenSinkTester.java
+++ sample-spring-boot-kitchensink/src/test/java/com/example/bot/spring/KitchenSinkTester.java
@@ -74,4 +74,32 @@ public class KitchenSinkTester {
 		assertThat(!thrown).isEqualTo(true);
 		assertThat(result).isEqualTo("def");
 	}
+	
+	@Test
+	public void testFound2() throws Exception {
+		boolean thrown = false;
+		String result = null;
+		try {
+			result = this.databaseEngine.search("I am fine");
+		} catch (Exception e) {
+			thrown = true;
+		}
+		assertThat(!thrown);
+		assertThat(result.contains("Great!"));
+	}
+	
+	@Test
+	public void testPartialFound() throws Exception {
+		boolean thrown = false;
+		String result = null;
+		try {
+			result = this.databaseEngine.search("Do you know what comes after abc?");
+		} catch (Exception e) {
+			thrown = true;
+		}
+		assertThat(!thrown);
+		assertThat(result.contains("def"));
+	}
+}
+
 }
