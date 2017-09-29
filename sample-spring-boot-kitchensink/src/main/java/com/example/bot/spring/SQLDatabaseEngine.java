f3002038835d1a6f58d64182dd054e28e07f41b6
httpak <httpak@connect.ust.hk>
2017-09-29 09:13:06 +0800

    I forgot what have I done

---
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/SQLDatabaseEngine.java
@@ -0,0 +1,45 @@
+package com.example.bot.spring;
+
+import lombok.extern.slf4j.Slf4j;
+import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
+import java.sql.*;
+import java.net.URISyntaxException;
+import java.net.URI;
+
+@Slf4j
+public class SQLDatabaseEngine extends DatabaseEngine {
+	@Override
+	String search(String text) throws Exception {
+		//Write your code here
+Connection connection = getConnection();
+		PreparedStatement stmt = connection.prepareStatement("SELECT keyword, response FROM lab3 where name like concat('%', ?, '%')");
+		stmt.setString(1, "vin");
+		ResultSet rs = stmt.executeQuery();
+		while (rs.next()){
+			String answer = rs.getString(2));
+		}
+		rs.close();
+		stmt.close();
+		connection.close();
+		return answer;
+	}
+	
+	
+	private Connection getConnection() throws URISyntaxException, SQLException {
+		Connection connection;
+		URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+		String username = dbUri.getUserInfo().split(":")[0];
+		String password = dbUri.getUserInfo().split(":")[1];
+		String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() +  "?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory";
+
+		log.info("Username: {} Password: {}", username, password);
+		log.info ("dbUrl: {}", dbUrl);
+		
+		connection = DriverManager.getConnection(dbUrl, username, password);
+
+		return connection;
+	}
+
+}
